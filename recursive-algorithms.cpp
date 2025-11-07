9e2b3be06b43c12ad432fc8611d44aa443a8fa1a
Roman666ny <csalwa515@gmail.com>
2025-11-07 16:43:32 +0300

    Create recursive-algorithms.cpp

---
+++ recursive-algorithms.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int sum_of_digits(int n) {
+    // Базовый случай
+    if (n < 10) {
+        return n;
+    }
+    // Рекурсивный случай
+    return n % 10 + sum_of_digits(n / 10);
+}
+
+int main() {
+    int num = 12345;
+    cout << "Сумма цифр числа " << num << " = " << sum_of_digits(num) << endl;
+    return 0;
+}
