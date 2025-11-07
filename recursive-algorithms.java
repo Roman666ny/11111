16bba7eb788288143cf5c572043f1c9e3d13382b
Roman666ny <csalwa515@gmail.com>
2025-11-07 16:44:05 +0300

    Create recursive-algorithms.java

---
+++ recursive-algorithms.java
@@ -0,0 +1,15 @@
+public class SumOfDigits {
+    public static int sumOfDigits(int n) {
+        // Базовый случай
+        if (n < 10) {
+            return n;
+        }
+        // Рекурсивный случай
+        return n % 10 + sumOfDigits(n / 10);
+    }
+    
+    public static void main(String[] args) {
+        int num = 12345;
+        System.out.println("Сумма цифр числа " + num + " = " + sumOfDigits(num));
+    }
+}
